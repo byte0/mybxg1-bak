35208e2e7ee07d761efb1321b180ed18c6259778
code1031 <wangzijie1031@qq.com>
2017-08-27 18:18:05 +0800

    完成退出功能

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php 
+/*
+  /main/index
+  /main/login
+
+  /teacher/add
+  /teacher/list
+
+  变量命名方式
+  分支结构
+  字符串拼接.
+  include关键字作用：嵌入子页面
+  常用api
+  1、array_key_exists() 判断数组中是否包含指定的key
+  2、substr() 截取字符串
+  3、explode() 分割字符串
+  4、count() 统计数组的长度
+  5、数组的基本使用
+
+  echo 
+  print_r()
+  var_dump()
+*/
   
   $dir = 'main'; // 默认目录名称
   $filename = 'index'; // 默认文件名称
@@ -10,7 +32,7 @@
     $path = $_SERVER['PATH_INFO'];
     // 去掉路径中的第一个斜杠
     $str = substr($path,1); // main/index
-    // 分割路径和文件名称
+    // 分割路径和文件名称(该方法与js中split类似)
     $arr = explode('/',$str);
     if(count($arr) == 2){
       $dir = $arr[0];// 覆盖目录名称
@@ -27,6 +49,5 @@
   //  /teacher/add
   // 在当前页码嵌入一个子页面
   include('./views/'.$dir.'/'.$filename.'.html');
-
 
  ?>
--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,21 @@
+NProgress.start();
+NProgress.done();
 
-	NProgress.start();
+$('.navs ul').prev('a').on('click', function () {
+	$(this).next().slideToggle();
+});
 
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+// 退出功能
+$('#logoutBtn').click(function(){
+  console.log(123);
+  $.ajax({
+    type : 'post',
+    url : '/api/logout',
+    dataType : 'json',
+    success : function(data){
+      if(data.code == 200){
+        location.href = '/main/login';
+      }
+    }
+  });
+});
