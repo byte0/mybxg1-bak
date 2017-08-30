8cf566c2d479ec1f3f10648f58c19e1359f9d85f
code1031 <wangzijie1031@qq.com>
2017-08-30 17:30:41 +0800

    讲师查看功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,4 +1,4 @@
-define(['jquery','template'],function($,template){
+define(['jquery','template','bootstrap'],function($,template){
   // 调用后台接口获取列表数据
   $.ajax({
     type : 'get',
@@ -8,6 +8,30 @@ define(['jquery','template'],function($,template){
       // 解析数据渲染页面
       var html = template('teacherTpl',{list : data.result});
       $('#teacherInfo').html(html);
+
+      // 绑定预览单击事件
+      $('.preview').click(function(){
+        // 获取当前记录ID
+        var td = $(this).closest('td');
+        var tcId = td.attr('data-tcId');
+        // 根据ID查询数据
+        $.ajax({
+          type : 'get',
+          url : '/api/teacher/view',
+          data : {tc_id : tcId},
+          dataType : 'json',
+          success : function(data){
+            // 解析数据渲染页面
+            var html = template('modalTpl',data.result);
+            $('#modalInfo').html(html);
+            // 显示弹窗
+            $('#teacherModal').modal();
+          }
+        });
+
+      });
     }
   });
+
+
 });
