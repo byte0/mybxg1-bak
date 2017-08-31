b01a2d883979d48cfb9a991d68b0e62576feb582
code1031 <wangzijie1031@qq.com>
2017-08-31 16:06:55 +0800

    完成讲师添加和编辑功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -15,12 +15,32 @@ define(['jquery','template','util'],function($,template,util){
         data.result.operate = '编辑讲师';
         var html = template('teacherTpl',data.result);
         $('#teacherInfo').html(html);
+        // 提交编辑讲师表单
+        submitForm('/api/teacher/update');
       }
     });
   }else{
     // 添加操作
     var html = template('teacherTpl',{operate : '添加讲师',tc_gender : 1});
     $('#teacherInfo').html(html);
+    // 提交添加讲师表单
+    submitForm('/api/teacher/add');
+  }
+  // 提交表单公用方法
+  function submitForm(url){
+    $('#teacherBtn').click(function(){
+      $.ajax({
+        type : 'post',
+        url : url,
+        data : $('#teacherForm').serialize(),
+        dataType : 'json',
+        success : function(data){
+          if(data.code == 200){
+            location.href = '/teacher/list';
+          }
+        }
+      });      
+    });
   }
 
 
