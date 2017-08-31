1270a131586f067a878d94782c7aa1dff2880c93
code1031 <wangzijie1031@qq.com>
2017-08-31 17:16:45 +0800

    添加表单验证插件

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','datepicker','language'],function($,template,util){
+define(['jquery','template','util','datepicker','language','validate'],function($,template,util){
   // 设置菜单选中
   util.setMenu('/teacher/list');
   // 获取编辑的讲师ID
@@ -28,20 +28,44 @@ define(['jquery','template','util','datepicker','language'],function($,template,
   }
   // 提交表单公用方法
   function submitForm(url){
-    $('#teacherBtn').click(function(){
-      $.ajax({
-        type : 'post',
-        url : url,
-        data : $('#teacherForm').serialize(),
-        dataType : 'json',
-        success : function(data){
-          if(data.code == 200){
-            location.href = '/teacher/list';
-          }
+    $('#teacherForm').validate({
+      sendForm : false,
+      valid : function(){
+        // 这里应该提交表单
+        
+      },
+      description : {
+        tc_name : {
+          required : '请输入用户名',
+          valid : '用户名可以使用'
+        },
+        tc_pass : {
+          required : '请输入密码',
+          pattern : '密码必须是6位数字',
+          valid : '密码有效'
+        },
+        tc_join_date : {
+          required : '请输入日期',
+          valid : '日期有效'
         }
-      });      
+      }
     });
   }
+  // function submitForm(url){
+  //   $('#teacherBtn').click(function(){
+  //     $.ajax({
+  //       type : 'post',
+  //       url : url,
+  //       data : $('#teacherForm').serialize(),
+  //       dataType : 'json',
+  //       success : function(data){
+  //         if(data.code == 200){
+  //           location.href = '/teacher/list';
+  //         }
+  //       }
+  //     });      
+  //   });
+  // }
 
 
 });
