644d29784a078ba99ef16085d3fd5af4dabcab48
code1031 <wangzijie1031@qq.com>
2017-09-05 17:42:18 +0800

    编辑和添加课时共用模态框

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','bootstrap'],function($,template,util){
   // 设置导航菜单选中
   util.setMenu('/course/list');
   // 获取课程ID
@@ -13,7 +13,30 @@ define(['jquery','template','util'],function($,template,util){
       // 渲染数据
       var html = template('lessonTpl',data.result);
       $('#lessonInfo').html(html);
-      
+      // 处理添加功能
+      $('#addBtn').click(function(){
+        var html = template('modalTpl',{operate: '添加课时'});
+        $('#modalInfo').html(html);
+        $('#chapterModal').modal();
+      });
+      // 处理编辑功能
+      $('.editLesson').click(function(){
+        // 先查询数据
+        $.ajax({
+          type : 'get',
+          url : '/api/course/chapter/edit',
+          data : {ct_id : $(this).attr('data-ctId')},
+          dataType :'json',
+          success : function(data){
+            // 渲染模板
+            data.result.operate = '编辑课时';
+            var html = template('modalTpl',data.result);
+            $('#modalInfo').html(html);
+            // 显示弹窗
+            $('#chapterModal').modal();
+          }
+        });
+      });
     }
   });
 });
