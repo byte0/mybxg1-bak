87cd538011f68cde9a76bb5412b1f18a3d4871f4
code1031 <wangzijie1031@qq.com>
2017-09-05 17:50:28 +0800

    完成编辑和添加课时的提交功能

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','bootstrap'],function($,template,util){
+define(['jquery','template','util','bootstrap','form'],function($,template,util){
   // 设置导航菜单选中
   util.setMenu('/course/list');
   // 获取课程ID
@@ -13,19 +13,43 @@ define(['jquery','template','util','bootstrap'],function($,template,util){
       // 渲染数据
       var html = template('lessonTpl',data.result);
       $('#lessonInfo').html(html);
+
+      // 表单提交功能
+      function submitForm(url,ctId){
+        var param = {ct_cs_id : csId};
+        if(ctId){
+          param.ct_id = ctId;
+        }
+        $('#submitBtn').click(function(){
+          $('#modalForm').ajaxSubmit({
+            type : 'post',
+            url : url,
+            data : param,
+            dataType : 'json',
+            success : function(data){
+              if(data.code == 200){
+                location.reload();
+              }
+            }
+          });
+        });
+      }
       // 处理添加功能
       $('#addBtn').click(function(){
         var html = template('modalTpl',{operate: '添加课时'});
         $('#modalInfo').html(html);
         $('#chapterModal').modal();
+        // 添加提交表单
+        submitForm('/api/course/chapter/add');
       });
       // 处理编辑功能
       $('.editLesson').click(function(){
         // 先查询数据
+        var ctId = $(this).attr('data-ctId');
         $.ajax({
           type : 'get',
           url : '/api/course/chapter/edit',
-          data : {ct_id : $(this).attr('data-ctId')},
+          data : {ct_id : ctId},
           dataType :'json',
           success : function(data){
             // 渲染模板
@@ -34,6 +58,8 @@ define(['jquery','template','util','bootstrap'],function($,template,util){
             $('#modalInfo').html(html);
             // 显示弹窗
             $('#chapterModal').modal();
+            // 编辑课时提交表单
+            submitForm('/api/course/chapter/modify',ctId);
           }
         });
       });
